f1ee57b03144a15d9fba387c50ba7f90c08391e9
Naywasd <sermsub2549@gmail.com>
2026-01-09 11:07:42 +0700

    Update lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,29 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double sumpower = 0;
+    double geo = 1;
+    double harmo = 0;
+    double Max = A[0];
+    double Min = A[0];
+    
+    for (int i = 0; i < N; i++){
+        sum += A[i];
+        sumpower += A[i]*A[i];
+        geo *= A[i];
+        harmo += 1.0/A[i];
+        
+        if(A[i] > Max) Max = A[i];
+        if(A[i] < Min) Min = A[i];
+    }
+    
+    B[0] = sum/N;
+    B[1] = sqrt(sumpower/N - B[0]*B[0]);
+    B[2] = pow(geo,1.0/N);
+    B[3] = N/harmo;
+    B[4] = Max;
+    B[5] = Min;
+}
